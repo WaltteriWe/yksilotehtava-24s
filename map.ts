e438226a6612bcbf63ac9ab413185fa55184a4c3
Waltteri <waltteri.westerholm@gmail.com>
2024-10-17 01:42:25 +0300

    PWAPWEAPWAPWAPWAPWAPWAPWAPPWA

--- map.ts
+++
@@ -1,8 +0,0 @@
-import * as L from "leaflet";
-import "leaflet/dist/leaflet.css";
-
-var map = L.map("map").setView([60.22407579575061, 24.758567542678975], 30);
-
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "© OpenStreetMap contributors",
-}).addTo(map);
